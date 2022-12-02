00600add37e91725d350a943c5460ec36e9dcb4e
hello <hello>
2022-12-02 15:22:22 -0500

    more samples

--- pd-moon/pd-moon.bat
+++ pd-moon/pd-moon.bat
@@ -1,1 +1,1 @@
-start /B ..\..\BizHawk-2.8-win-x64\EmuHawk.exe --config=.\shoot+reload.ini --load-state=.\Moon.State --lua=".\pd-moon.lua" "..\..\roms\Perfect Dark (USA) (Rev A).n64"
+start /B ..\..\BizHawk-2.8-win-x64\EmuHawk.exe --config=.\shoot+reload.ini --load-state=.\wall-far.State --lua=".\pd-moon.lua" "..\..\roms\Perfect Dark (USA) (Rev A).n64"
