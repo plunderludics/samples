ff5519dacb5b407e37a0534b5694acee64534a63
hello <hello>
2023-02-25 10:24:03 -0500

    hide populous cursor!

--- populous-minimap/populous-minimap.lua
+++ populous-minimap/populous-minimap.lua
@@ -1,6 +1,26 @@
+cursor_x_addr = 0x164724
+
+map_left  = 14155776
+map_right = 20447232
+
 function main()
     while true do
         emu.frameadvance();
+        x = memory.read_u32_le(cursor_x_addr)
+
+        -- make the cursor skip horizontally over the minimap
+        pressed = joypad.getimmediate()
+
+        if (map_left < x and x < map_right) then
+            if (pressed["P1 D-Pad Right"]) then
+                -- skip to right edge
+                new_x = map_right
+            else
+                new_x = map_left
+            end
+            
+            memory.write_u32_le(cursor_x_addr, new_x)
+        end
 
         -- Directional only
         joypad.set({
@@ -19,7 +39,7 @@ function main()
         }, 1)
 
 		-- gui.clearGraphics();
-        -- gui.drawText(100, 40, x..","..y);
+        -- gui.drawText(10, 10, map_left.."\n"..x.."\n"..map_right);
         -- print("hi");
 	end
 end
